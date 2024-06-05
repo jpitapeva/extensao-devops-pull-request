68590b62158bc029f111a537cc3959311ffbd3ae
joaopma <joaopaulo.m@terra.com.br>
2024-06-05 20:45:19 -0300

    in

---
+++ JPCompcombr/review.ts
@@ -0,0 +1,80 @@
+import fetch from 'node-fetch';
+import { git } from './git';
+import { OpenAIApi } from 'openai';
+import { addCommentToPR } from './pr';
+import { Agent } from 'https';
+import * as tl from "azure-pipelines-task-lib/task";
+
+export async function reviewFile(targetBranch: string, fileName: string, httpsAgent: Agent, apiKey: string, openai: OpenAIApi | undefined, aoiEndpoint: string | undefined) {
+  console.log(`Start reviewing ${fileName} ...`);
+
+  const defaultOpenAIModel = 'gpt-3.5-turbo';
+  const patch = await git.diff([targetBranch, '--', fileName]);
+
+  const instructions = `Act as a code reviewer of a Pull Request, providing feedback on possible bugs and clean code issues.
+        You are provided with the Pull Request changes in a patch format.
+        Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.
+
+        As a code reviewer, your task is:
+                - Review only added, edited or deleted lines.
+                - If there's no bugs and the changes are correct, write only 'No feedback.'
+                - If there's bug or uncorrect code changes, don't write 'No feedback.'`;
+
+  try {
+    let choices: any;
+
+    if (openai) {
+      const response = await openai.createChatCompletion({
+        model: tl.getInput('model') || defaultOpenAIModel,
+        messages: [
+          {
+            role: "system",
+            content: instructions
+          },
+          {
+            role: "user",
+            content: patch
+          }
+        ],
+        max_tokens: 500
+      });
+
+      choices = response.data.choices
+    }
+    else if (aoiEndpoint) {
+      const request = await fetch(aoiEndpoint, {
+        method: 'POST',
+        headers: { 'api-key': `${apiKey}`, 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          max_tokens: 500,
+          messages: [{
+            role: "user",
+            content: `${instructions}\n, patch : ${patch}}`
+          }]
+        })
+      });
+
+      const response = await request.json();
+
+      choices = response.choices;
+    }
+
+    if (choices && choices.length > 0) {
+      const review = choices[0].message?.content as string;
+
+      if (review.trim() !== "No feedback.") {
+        await addCommentToPR(fileName, review, httpsAgent);
+      }
+    }
+
+    console.log(`Review of ${fileName} completed.`);
+  }
+  catch (error: any) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
+}
